5e8f45de5c0c6d55cfb16c7292a54f764b370795
purpurlu <laurazurbayeva@gmail.com>
2022-07-18 01:29:19 +0100

    change food

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -9,7 +9,7 @@ function MainContent(): JSX.Element {
   const numOne = 3;
   const numTwo = 6;
 
-  const threeFavouriteFoods = ["Chocolate", "Noodles", "Pizza"];
+  const threeFavouriteFoods = ["Tiramisu", "Manty", "Pizza"];
 
   return (
     <main>
